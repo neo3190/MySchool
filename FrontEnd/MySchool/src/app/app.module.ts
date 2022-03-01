7e4820a56f89ef5fdb7e0a349312615dfbf19a5f
neo3190 <neo3190@gmail.com>
2022-03-01 15:20:49 +0530

    ui

--- FrontEnd/MySchool/src/app/app.module.ts
+++ FrontEnd/MySchool/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { AngularMaterialModule } from './angular-material.module';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
